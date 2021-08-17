c9b520730f296270a154aa1c49569403e5fe380d
Jhony Vidal <mrjhonyvidal@gmail.com>
2021-08-17 00:53:13 -0300

    Add Language selection on login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:aipetto/bloc/user_geolocation/user_geolocation_bloc.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'routes/route_generator.dart';
--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -1,13 +1,68 @@
+import 'package:aipetto/data/pref_manager.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
-import '../../components/custom_button.dart';
 import '../../components/wave_header.dart';
-import '../../routes/routes.dart';
 import 'widgets/input_widget.dart';
 import 'widgets/social_login_widget.dart';
 
-class LoginPage extends StatelessWidget {
+/// TODO apply architecture best practices and DDD extracting this to own language module
+enum Language { english, spanish, italian, portuguese }
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  var _language;
+  @override
+  void initState() {
+    super.initState();
+
+    switch (Prefs.getString('language', def: 'es')) {
+      case 'en':
+        _language = Language.english;
+        break;
+
+      case 'es':
+        _language = Language.spanish;
+        break;
+
+      case 'pt':
+        _language = Language.portuguese;
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  _changeLanguage(int index) {
+    switch (index) {
+      case 0:
+        _language = Language.english;
+        Prefs.setString(Prefs.LANGUAGE, 'en');
+        Prefs.setString(Prefs.LANGUAGE_REGION, 'UK');
+        break;
+
+      case 1:
+        _language = Language.spanish;
+        Prefs.setString(Prefs.LANGUAGE, 'es');
+        Prefs.setString(Prefs.LANGUAGE_REGION, 'AR');
+        break;
+
+      case 2:
+        _language = Language.portuguese;
+        Prefs.setString(Prefs.LANGUAGE, 'pt');
+        Prefs.setString(Prefs.LANGUAGE_REGION, 'BR');
+        break;
+    }
+    //print("language" + _language.toString());
+    context.setLocale(Locale(Prefs.getString(Prefs.LANGUAGE), Prefs.getString(Prefs.LANGUAGE_REGION)));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,6 +99,34 @@ class LoginPage extends StatelessWidget {
                                   fontWeight: FontWeight.w700,
                                 ),
                               ),
+                            ),
+                            RadioListTile(
+                              value: Language.english,
+                              onChanged: (value) => _changeLanguage(0),
+                              groupValue: _language,
+                              title: Text('english'.tr()),
+                            ),
+                            Divider(
+                              height: 0.5,
+                              indent: 10,
+                              endIndent: 10,
+                            ),
+                            RadioListTile(
+                              value: Language.spanish,
+                              onChanged: (value) => _changeLanguage(1),
+                              groupValue: _language,
+                              title: Text('spanish'.tr()),
+                            ),
+                            Divider(
+                              height: 0.5,
+                              indent: 10,
+                              endIndent: 10,
+                            ),
+                            RadioListTile(
+                              value: Language.portuguese,
+                              onChanged: (value) => _changeLanguage(2),
+                              groupValue: _language,
+                              title: Text('portuguese'.tr()),
                             ),
                             SizedBox(
                               height: 30,

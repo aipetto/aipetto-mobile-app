ff37921208ca9f46c9f3b49b61bced3e5f151760
Jhony Vidal <mrjhonyvidal@gmail.com>
2021-09-10 00:07:28 -0300

    Merge changes locally v1.0.9-feature-alpha with Add new Pet

--- lib/modules/pet/widgets/new_pet_widget.dart
+++ lib/modules/pet/widgets/new_pet_widget.dart
@@ -8,6 +8,7 @@ import 'package:aipetto/routes/routes.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:image_picker/image_picker.dart';
 
 import '../../../components/text_form_field.dart';
 import '../../../utils/constants.dart';
